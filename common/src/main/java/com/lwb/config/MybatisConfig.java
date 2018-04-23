6cc39da6f9f742816c787fa5417ee6651780f820
06112497liu <804912547@qq.com>
2018-04-23 20:33:56 +0800

    jdk8学习

---
+++ common/src/main/java/com/lwb/config/MybatisConfig.java
@@ -0,0 +1,7 @@
+package com.lwb.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisConfig {
+}
---
+++ common/src/main/java/com/lwb/config/properties/JDBCProperties.java
@@ -0,0 +1,62 @@
+package com.lwb.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jdbc")
+public class JDBCProperties {
+
+    /**
+     * 数据库驱动
+     */
+    private String driverClassName;
+
+    /**
+     * 数据库地址
+     */
+    private String url;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
