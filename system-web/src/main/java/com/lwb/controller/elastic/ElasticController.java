890862752bc2857aeacb65618bfc0a2ad6af6bad
abnormal <804912547@qq.com>
2018-05-07 09:07:13 +0800

    Update ElasticController.java

--- system-web/src/main/java/com/lwb/controller/elastic/ElasticController.java
+++ system-web/src/main/java/com/lwb/controller/elastic/ElasticController.java
@@ -1,26 +1,26 @@
-//package com.lwb.controller;
-//
-//import com.lwb.service.ElasticService;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.annotation.Resource;
-//
-///**
-// * @Author liuweibo
-// * @Date 2018/4/4
-// */
-//@RestController
-//@RequestMapping("elastic/")
-//public class ElasticController {
-//
-//    @Resource
-//    private ElasticService elasticService;
-//
-//    @GetMapping("test01")
-//    public void test01() {
-//        elasticService.hello();
-//    }
-//
-//}
+package com.lwb.controller;
+
+import com.lwb.service.ElasticService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author liuweibo
+ * @Date 2018/4/4
+ */
+@RestController
+@RequestMapping("elastic/")
+public class ElasticController {
+
+    @Resource
+    private ElasticService elasticService;
+
+    @GetMapping("test01")
+    public void test01() {
+        elasticService.hello();
+    }
+
+}
