95a6fb976e17f1f3982343d257b5a590721bb98d
abnormal <804912547@qq.com>
2018-05-09 15:11:12 +0800

    Update TopicProducerImpl.java

--- system-service/src/main/java/com/lwb/service/mq/producer/impl/TopicProducerImpl.java
+++ system-service/src/main/java/com/lwb/service/mq/producer/impl/TopicProducerImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +16,12 @@ public class TopicProducerImpl implements TopicProducer {
     private static final Logger LOGGER = LoggerFactory.getLogger(TopicProducerImpl.class);
 
     @Autowired
-    private JmsMessagingTemplate jmsMessagingTemplate;
+    private JmsTemplate jmsTemplate;
 
     @Override
     public void sendMsg(String destName, String msg) {
         this.LOGGER.info("推送消息：{} --> MQ的Topic：{}", msg, destName);
         ActiveMQTopic topic = new ActiveMQTopic(destName);
-        this.jmsMessagingTemplate.convertAndSend(topic, msg);
+        this.jmsTemplate.convertAndSend(topic, msg);
     }
 }
