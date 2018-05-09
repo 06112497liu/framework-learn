f92268a4b132d858232a477da4c1f6d614bd96e7
abnormal <804912547@qq.com>
2018-05-09 15:13:17 +0800

    Update StartApplication.java

--- system-web/src/main/java/com/lwb/StartApplication.java
+++ system-web/src/main/java/com/lwb/StartApplication.java
@@ -3,6 +3,7 @@ package com.lwb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.web.bind.annotation.RestController;
 
