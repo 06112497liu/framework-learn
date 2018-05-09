79c1c3b0741bb4df5d82e4b173844ca33526daca
abnormal <804912547@qq.com>
2018-05-09 15:10:09 +0800

    Update Consumer.java

--- system-service/src/main/java/com/lwb/service/mq/consumer/Consumer.java
+++ system-service/src/main/java/com/lwb/service/mq/consumer/Consumer.java
@@ -5,10 +5,14 @@ import com.lwb.constant.TopicList;
 import org.apache.activemq.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
 
 
 /**
@@ -20,14 +24,21 @@ public class Consumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
 
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
     /**
-     * @JmsListener 这个注解可重复，这样一个消费者可以消费多个queue或者多个topic的消息
      * @param msg 这个msg的类型可以是很多中类型，甚至可以是单纯的string，是string的话，就是获取的消息的具体内容
+     * @JmsListener 这个注解可重复，这样一个消费者可以消费多个queue或者多个topic的消息
      */
     @JmsListener(destination = QueueList.BUSINESS2)
-    @JmsListener(destination = QueueList.BUSINESS1)
-    public void topicListener1(Message msg) {
+    public void topicListener1(Message msg) throws JMSException {
         LOGGER.info("topicListener1 监听到MQ：{} 对列的消息：{}", QueueList.BUSINESS1, msg);
+    }
+
+    @JmsListener(destination = QueueList.BUSINESS2)
+    public void topicListener11(Message msg) throws JMSException {
+        LOGGER.info("topicListener11 监听到MQ：{} 对列的消息：{}", QueueList.BUSINESS1, msg);
     }
 
     @JmsListener(destination = QueueList.BUSINESS1)
